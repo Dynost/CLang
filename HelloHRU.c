634cd401814f192eb74f496ce8b8d844718b2774
Kraze <frostop508@gmail.com>
2026-05-29 10:52:06 +0530

    day 2 first commit

--- HelloHRU.c
+++ HelloHRU.c
@@ -1,5 +1,20 @@
 #include <stdio.h>
-int main() {
-    printf("Hello, HRU!\n");
-    return 0;
+int main()
+{
+    int var;       /* declare var */
+    var = 60;      /* assign or initialize value to var */
+    int var2 = 70; /* declare var2 and assign/initialize value */
+
+    // Q: Camel case example
+    // A: camelCaseExample (Starts with lowercase letter, and each subsequent word starts with an uppercase letter)
+    // Q: Snake case example
+    // A: snake_case_example (All letters are lowercase, and words are separated by underscores)
+    // Q: Pascal case example
+    // A: PascalCaseExample (Each word starts with an uppercase letter, including the first
+    
+    // Format specifiers:
+    printf("Value of var: %d\n", var);
+    printf("Value of var2: %d\n", var2);
+
+    return 0; 
 }
